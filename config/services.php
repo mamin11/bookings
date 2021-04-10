85e97d2a0268fdede9812a6d389895bf18fcae7d
mamin11 <abdimamin@gmail.com>
2021-04-10 23:14:41 +0100

    login with google callback edited

--- config/services.php
+++ config/services.php
@@ -33,7 +33,7 @@ return [
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect' => 'http://127.0.0.1:8000/google/callback',
+        'redirect' => 'https://dissertation-amin.herokuapp.com/google/callback',
     ],
 
     'stripe' => [
